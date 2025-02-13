e90461dd157ba0da6405f0d99a42175f2194d698
1Manisundar <chsundar143@gmail.com>
2025-02-13 22:15:46 +0530

    Created Header,user for taskmanagement

---
+++ Angular/01-start/src/app/fake-users.ts
@@ -0,0 +1,26 @@
+export const FAKE_USERS =[
+    {
+        id:'u1',
+        name: 'Hemanth',
+        icon: 'icon-1.jpg',
+    },
+    {
+        id:'u2',
+        name: 'Venkat',
+        icon: 'icon-2.jpg',
+    },{
+        id:'u1',
+        name: 'Naga',
+        icon: 'icon-3.jpg',
+    },
+    {
+        id:'u1',
+        name: 'Mani',
+        icon: 'icon-4.jpg',
+    },
+    {
+        id:'u1',
+        name: 'Sundar',
+        icon: 'icon-5.jpg',
+    },
+]
